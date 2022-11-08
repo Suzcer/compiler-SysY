6ba4552911c36af5dc0d1d28963244c2815a5eae
Suzcer <201250104@smail.nju.edu.cn>
2022-11-08 04:06:18 -0800

    submit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,17 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class Main
 {    
-    public static void main(String[] args){}
+    public static void main(String[] args){
+        try (Scanner sc = new Scanner(new FileReader(args[0]))) {
+            while (sc.hasNextLine()) {  //按行读取字符串
+                String line = sc.nextLine();
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
