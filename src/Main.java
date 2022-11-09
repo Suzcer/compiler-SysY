17e55f107114178eb0143a5fdfafa91220ef1eb8
Suzcer <201250104@smail.nju.edu.cn>
2022-11-09 03:07:23 -0800

    ..

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.io.FileReader;
 import java.util.Scanner;
 
 public class Main
-{    
+{
     public static void main(String[] args){
         try (Scanner sc = new Scanner(new FileReader(args[0]))) {
             while (sc.hasNextLine()) {  //按行读取字符串
