43db4c0d6c20050fc4eb017a85d677340b1bcb32
tracer-compiler2022 <compilers@software.nju.edu.cb>
2022-11-09 18:00:17 -0800

    > make 201250104 root Linux ubuntu 5.15.0-52-generic #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:00:17 up 22 min,  1 user,  load average: 0.75, 0.41, 0.32 8fb34815441fc0a9f5868622e2a0272266c616eb

--- src/SysYLexer.java
+++ src/SysYLexer.java
@@ -1,4 +1,4 @@
-// Generated from /usr/local/lib/Lab/src/SysYLexer.g4 by ANTLR 4.10.1
+// Generated from ./src/SysYLexer.g4 by ANTLR 4.10.1
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.Token;
