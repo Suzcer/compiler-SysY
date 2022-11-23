bbffdcec7acabf6c1ce04ecb9e5cdcc693c2e78f
tracer-compiler2022 <compilers@software.nju.edu.cb>
2022-11-22 19:05:25 -0800

    > make 201250104 root Linux ubuntu 5.15.0-53-generic #59~20.04.1-Ubuntu SMP Thu Oct 20 15:10:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:05:25 up 1 day, 20:00,  1 user,  load average: 0.53, 0.43, 0.40 129c21fe69abc2c75f40621c3f8c69ededda8169

--- src/Visitor.java
+++ src/Visitor.java
@@ -34,7 +34,7 @@ public class Visitor extends SysYParserBaseVisitor<Void>{
     public Void visitChildren(RuleNode node) {
         String ruleName = ruleNames[node.getRuleContext().getRuleIndex()];
         for(int i=0;i+1<node.getRuleContext().depth();i++) System.out.print("  ");
-        System.out.println(captureName(ruleName));
+        System.err.println(captureName(ruleName));
         return super.visitChildren(node);
     }
 
@@ -48,12 +48,12 @@ public class Visitor extends SysYParserBaseVisitor<Void>{
             if(node.getParent() instanceof RuleNode){
                 parent = (RuleNode) node.getParent();
                 for(int i=0;i<parent.getRuleContext().depth();i++)
-                    System.out.print("  ");
+                    System.err.print("  ");
             }
 
-            System.out.print(node.getText()+" ");
-            System.out.print(type);
-            System.out.println("["+mp.get(type)+"]");
+            System.err.print(node.getText()+" ");
+            System.err.print(type);
+            System.err.println("["+mp.get(type)+"]");
         }
 
         return super.visitTerminal(node);
