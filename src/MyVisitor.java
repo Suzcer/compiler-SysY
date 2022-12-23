e43e3dc810d204c0c332fccd7f5e1ad698db6af9
tracer-compiler2022 <compilers@software.nju.edu.cb>
2022-12-22 22:08:32 -0800

    > make 201250104 root Linux ubuntu 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:08:32 up 1 day,  3:44,  2 users,  load average: 0.50, 0.80, 0.77 558d28d1ac3f029453e01b30b2d4811819812c5d

--- src/MyVisitor.java
+++ src/MyVisitor.java
@@ -1,56 +1,56 @@
-import org.bytedeco.javacpp.Pointer;
-import org.bytedeco.javacpp.PointerPointer;
-import org.bytedeco.llvm.LLVM.*;
-
-import static org.bytedeco.llvm.global.LLVM.*;
-
-
-public class MyVisitor extends SysYParserBaseVisitor<LLVMValueRef>{
-
-    LLVMModuleRef module;
-    LLVMBuilderRef builder;
-    LLVMTypeRef i32Type;
-    LLVMValueRef zero;
-
-    public MyVisitor(){
-
-        //1. 初始化LLVM
-        LLVMInitializeCore(LLVMGetGlobalPassRegistry());
-        LLVMLinkInMCJIT();
-        LLVMInitializeNativeAsmPrinter();
-        LLVMInitializeNativeAsmParser();
-        LLVMInitializeNativeTarget();
-
-        //2. 创建module
-        module = LLVMModuleCreateWithName("module");
-
-        //3. 初始化IRBuilder，后续将使用这个builder去生成LLVM IR
-        builder = LLVMCreateBuilder();
-
-        //4. 考虑到我们的语言中仅存在int一个基本类型，可以通过下面的语句为LLVM的int型重命名方便以后使用
-        i32Type = LLVMInt32Type();
-        zero = LLVMConstInt(i32Type, 0, 0);
-
-        PointerPointer<Pointer> mainParamTypes = new PointerPointer<>(0);
-
-        LLVMTypeRef mainRetType = LLVMFunctionType(i32Type, mainParamTypes, 0, 0);
-
-        LLVMValueRef mainFunction = LLVMAddFunction(module, "main", mainRetType);
-
-        LLVMBasicBlockRef mainEntry = LLVMAppendBasicBlock(mainFunction, "");
-        LLVMPositionBuilderAtEnd(builder, mainEntry);
-
-        LLVMValueRef alloca1 = LLVMBuildAlloca(builder, i32Type, "");
-
-        LLVMBuildStore(builder, zero, alloca1);
-    }
-
-    @Override
-    public LLVMValueRef visitProgram(SysYParser.ProgramContext ctx) {
-
-        return super.visitProgram(ctx);
-    }
-
-
-
-}
+//import org.bytedeco.javacpp.Pointer;
+//import org.bytedeco.javacpp.PointerPointer;
+//import org.bytedeco.llvm.LLVM.*;
+//
+//import static org.bytedeco.llvm.global.LLVM.*;
+//
+//
+//public class MyVisitor extends SysYParserBaseVisitor<LLVMValueRef>{
+//
+//    LLVMModuleRef module;
+//    LLVMBuilderRef builder;
+//    LLVMTypeRef i32Type;
+//    LLVMValueRef zero;
+//
+//    public MyVisitor(){
+//
+//        //1. 初始化LLVM
+//        LLVMInitializeCore(LLVMGetGlobalPassRegistry());
+//        LLVMLinkInMCJIT();
+//        LLVMInitializeNativeAsmPrinter();
+//        LLVMInitializeNativeAsmParser();
+//        LLVMInitializeNativeTarget();
+//
+//        //2. 创建module
+//        module = LLVMModuleCreateWithName("module");
+//
+//        //3. 初始化IRBuilder，后续将使用这个builder去生成LLVM IR
+//        builder = LLVMCreateBuilder();
+//
+//        //4. 考虑到我们的语言中仅存在int一个基本类型，可以通过下面的语句为LLVM的int型重命名方便以后使用
+//        i32Type = LLVMInt32Type();
+//        zero = LLVMConstInt(i32Type, 0, 0);
+//
+//        PointerPointer<Pointer> mainParamTypes = new PointerPointer<>(0);
+//
+//        LLVMTypeRef mainRetType = LLVMFunctionType(i32Type, mainParamTypes, 0, 0);
+//
+//        LLVMValueRef mainFunction = LLVMAddFunction(module, "main", mainRetType);
+//
+//        LLVMBasicBlockRef mainEntry = LLVMAppendBasicBlock(mainFunction, "");
+//        LLVMPositionBuilderAtEnd(builder, mainEntry);
+//
+//        LLVMValueRef alloca1 = LLVMBuildAlloca(builder, i32Type, "");
+//
+//        LLVMBuildStore(builder, zero, alloca1);
+//    }
+//
+//    @Override
+//    public LLVMValueRef visitProgram(SysYParser.ProgramContext ctx) {
+//
+//        return super.visitProgram(ctx);
+//    }
+//
+//
+//
+//}
